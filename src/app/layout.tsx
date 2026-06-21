fc7aacc7eb6a86c642615cfb126f5e4e9209c6ea
sheikmd1804 <161332821+sheikmd1804@users.noreply.github.com>
2026-06-21 16:46:07 +0530

    add Category DB model

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,9 @@ import type { Metadata } from "next";
 import { Inter, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import HomeNav from "@/components/homeNav";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,8 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("font-sans", geist.variable)}
+    >
       <body className={inter.className} suppressHydrationWarning>
+        <HomeNav />
         {children}
       </body>
     </html>
